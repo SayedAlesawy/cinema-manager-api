4493916c22c29c513dacd24ff207f8c6c3d5d9b8
SayedAlesawy <sayedalesawy14@gmail.com>
2019-12-20 22:11:42 +0200

    rescue from common exceptions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,22 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound do |_exception|
+    render json: { message: 'record not found' }, status: 404
+  end
+
+  rescue_from ActiveRecord::RecordNotSaved,
+              ActiveRecord::RecordInvalid do |exception|
+    render json: {
+      message: exception.record.errors.full_messages.join(', ')
+    }, status: 422
+  end
+
+  rescue_from ActiveRecord::NotNullViolation do |_exception|
+    render json: { message: 'parameters invalid' }, status: 422
+  end
+
+  rescue_from ActionController::ParameterMissing do |exception|
+    render json: { message: exception.message }, status: 400
+  end
 end
