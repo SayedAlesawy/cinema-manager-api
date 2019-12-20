8661f63a94182b75c4ce70b833ad11bc5d80d1a6
SayedAlesawy <sayedalesawy14@gmail.com>
2019-12-20 21:24:03 +0200

    added user model

---
+++ db/migrate/20191220183518_create_user.rb
@@ -0,0 +1,21 @@
+class CreateUser < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      # Handles authentication
+      t.string :email, null: false
+      t.string :password_digest
+
+      # User attributes
+      t.string :username, null: false
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.date :birthday
+
+      # Handles authorization
+      t.boolean :is_admin, default: false
+
+      # Handles timestamps
+      t.timestamps
+    end
+  end
+end
